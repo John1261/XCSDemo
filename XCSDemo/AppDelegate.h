0f0f687daa6a61caaeac17eb2f96be149e2990a5
John <>
2017-10-18 11:14:04 +0800

    init

---
+++ XCSDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCSDemo
+//
+//  Created by John on 2017/10/18.
+//  Copyright © 2017年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCSDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCSDemo
+//
+//  Created by John on 2017/10/18.
+//  Copyright © 2017年 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
